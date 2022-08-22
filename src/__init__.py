0f03acc7b2fa1f9d54aaf262b568af994ca9d9ee
irissonnlima <irisson2203@gmail.com>
2022-08-22 00:33:46 -0300

    Adição de estrutura básica para trabalho

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+__version__ = '0.1.0'
+class A():
+    def __init__(self, a:int, b:int):
+        self.a = a
+        self.b = b
+    def minus(self):
+        return self.a-self.b
+        
+class B(A):
+    def sum(self):
+        return self.a + self.b
+    
+
+value = A(1,2)
+value2 = B(3,4)
+
+print(value.minus())
+print(value2.sum())    
+
